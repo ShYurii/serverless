2476f1891908a0880681f7c4771562e3fe308b47
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-04-23 22:21:44 +0300

    task04 init commit

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -5,10 +5,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
-//import software.amazon.awssdk.services.sqs.model.Message;
 //import com.amazonaws.services.lambda.runtime.Context;
 //import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,20 +15,22 @@ import org.apache.logging.log4j.Logger;
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(lambdaName = "sqs_handler",
-	roleName = "sqs_handler-role",
+@LambdaHandler(lambdaName = "sns_handler",
+	roleName = "sns_handler-role",
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class SqsHandler implements RequestHandler<SQSEvent, Void> {
 
-	private static final Logger LOG = LogManager.getLogger(SqsHandler.class);
+public class SnsHandler implements RequestHandler<SNSEvent, Void> {
+
+	private static final Logger LOG = LogManager.getLogger(SnsHandler.class);
 
 	@Override
-	public Void handleRequest(SQSEvent event, Context context) {
-		for (SQSEvent.SQSMessage msg : event.getRecords()){
-			LOG.info("New message from SQS: " + msg.getBody());
+	public Void handleRequest(SNSEvent event, Context context) {
+		for (SNSEvent.SNSRecord record : event.getRecords()) {
+			LOG.info("New message from SNS: " + record.getSNS().getMessage());
 		}
 		return null;
 	}
+}
