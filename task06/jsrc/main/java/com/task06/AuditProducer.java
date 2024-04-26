5a39ca427811bf8dccdbdac1184a532a6ef214ac
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-04-26 23:40:04 +0300

    task06 init commit

--- task06/jsrc/main/java/com/task06/AuditProducer.java
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -5,8 +5,18 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStreamRecord;
+
+
 
 @LambdaHandler(lambdaName = "audit_producer",
 	roleName = "audit_producer-role",
@@ -14,13 +24,59 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class AuditProducer implements RequestHandler<Object, Map<String, Object>> {
+public class AuditProducer implements RequestHandler<DynamodbEvent, Void> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+	private static final String DYNAMODB_TABLE_NAME = "Audit";
+	private final DynamoDbClient client = DynamoDbClient.create();
+
+	@Override
+	public Void handleRequest(DynamodbEvent event, Context context) {
+		for (DynamodbStreamRecord record : event.getRecords()) {
+			if ("INSERT".equals(record.getEventName())) {
+				handleInsert(record);
+			} else if ("MODIFY".equals(record.getEventName())) {
+				handleModify(record);
+			} else if ("REMOVE".equals(record.getEventName())) {
+				handleRemove(record);
+			}
+		}
+		return null;
+	}
+
+	private void handleInsert(DynamodbStreamRecord record) {
+		Map<String, AttributeValue> item = new HashMap<>();
+		item.put("id", AttributeValue.builder().s(UUID.randomUUID().toString()).build());
+		item.put("itemKey", AttributeValue.builder().s(record.getDynamodb().getNewImage().get("key").getS()).build());
+		item.put("modificationTime", AttributeValue.builder().s(Instant.now().toString()).build());
+		item.put("newValue", AttributeValue.builder().s(record.getDynamodb().getNewImage().get("value").getN()).build());
+		putItem(item);
+	}
+
+	private void handleModify(DynamodbStreamRecord record) {
+		Map<String, AttributeValue> item = new HashMap<>();
+		item.put("id", AttributeValue.builder().s(UUID.randomUUID().toString()).build());
+		item.put("itemKey", AttributeValue.builder().s(record.getDynamodb().getNewImage().get("key").getS()).build());
+		item.put("modificationTime", AttributeValue.builder().s(Instant.now().toString()).build());
+		item.put("updatedAttribute", AttributeValue.builder().s("value").build());
+		item.put("oldValue", AttributeValue.builder().s(record.getDynamodb().getOldImage().get("value").getN()).build());
+		item.put("newValue", AttributeValue.builder().s(record.getDynamodb().getNewImage().get("value").getN()).build());
+		putItem(item);
+	}
+
+	private void handleRemove(DynamodbStreamRecord record) {
+		Map<String, AttributeValue> item = new HashMap<>();
+		item.put("id", AttributeValue.builder().s(UUID.randomUUID().toString()).build());
+		item.put("itemKey", AttributeValue.builder().s(record.getDynamodb().getOldImage().get("key").getS()).build());
+		item.put("modificationTime", AttributeValue.builder().s(Instant.now().toString()).build());
+		item.put("oldValue", AttributeValue.builder().s(record.getDynamodb().getOldImage().get("value").getN()).build());
+		putItem(item);
+	}
+
+	private void putItem(Map<String, AttributeValue> item) {
+		PutItemRequest putItemRequest = PutItemRequest.builder()
+				.tableName(DYNAMODB_TABLE_NAME)
+				.item(item)
+				.build();
+		client.putItem(putItemRequest);
 	}
 }
