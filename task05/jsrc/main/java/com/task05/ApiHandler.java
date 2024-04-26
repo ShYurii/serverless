ca4876541bb04cf94fa399695210f6b6060dfe2c
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-04-26 15:04:40 +0300

    task05 init commit

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -32,28 +32,21 @@ import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
         aliasName = "${lambdas_alias_name}",
         logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-
-
 public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
-    //	private static final String DYNAMODB_TABLE_NAME = "Events";
     private static final String DYNAMODB_TABLE_NAME = "cmtr-c5efef97-Events-test";
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
         DynamoDbClient client = DynamoDbClient.create();
-
         Map<String, Object> output = new HashMap<>();
 
         try {
             String id = UUID.randomUUID().toString();
-
             String createdAt = java.time.Clock.systemUTC().instant().toString();
 
             Map<String, AttributeValue> item = new HashMap<>();
             item.put("id", AttributeValue.builder().s(id).build());
-//            item.put("principalId", AttributeValue.builder().s(input.get("principalId").toString()).build());
             item.put("createdAt", AttributeValue.builder().s(createdAt).build());
 
             Object principalId = input.get("principalId");
@@ -62,50 +55,34 @@ public class ApiHandler implements RequestHandler<Map<String, Object>, Map<Strin
                 output.put("error", "Missing principalId");
                 return output;
             }
-//            item.put("principalId", AttributeValue.builder().s(principalId.toString()).build());
             item.put("principalId", AttributeValue.builder().n(principalId.toString()).build());
 
-            Object content = input.get("content");
+            Map<String, Object> content = (Map<String, Object>) input.get("content");
             if (content == null) {
                 output.put("statusCode", 400);
                 output.put("error", "Missing content");
                 return output;
             }
 
-
-// дополнительная обертка , чтобы пройти тест
-            Map<String, Object> body = new HashMap<>();
-//            body.put("content", content);
-            body.put("content", new HashMap<>());
-//            body.put("content", null);
-//            это правильно
-//            item.put("body", AttributeValue.builder().s(objectMapper.writeValueAsString(body)).build());
-//           чтобы пройти тест
-
-            ObjectMapper objectMapper = new ObjectMapper();
-//            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-            item.put("body", AttributeValue.builder().s(objectMapper.writeValueAsString(body)).build());
-//            item.put("body", AttributeValue.builder().s("any map '{' content '}").build());
-//            item.put("body", AttributeValue.builder().s(objectMapper.writeValueAsString(input.get("content"))).build());
+            Map<String, AttributeValue> body = new HashMap<>();
+            for (Map.Entry<String, Object> entry : content.entrySet()) {
+                body.put(entry.getKey(), AttributeValue.builder().s(entry.getValue().toString()).build());
+            }
+            item.put("body", AttributeValue.builder().m(body).build());
 
             PutItemRequest putItemRequest = PutItemRequest.builder()
                     .tableName(DYNAMODB_TABLE_NAME)
                     .item(item)
                     .build();
 
-            PutItemResponse putItemResponse = client.putItem(putItemRequest);
+            client.putItem(putItemRequest);
 
             output.put("statusCode", 201);
-//			output.put("event", item);
             output.put("event", Map.of(
                     "createdAt", item.get("createdAt").s(),
-//                    "principalId", item.get("principalId").s(),
-//                    "principalId", item.get("principalId").n(),
                     "principalId", Integer.parseInt(item.get("principalId").n()),
-
                     "id", item.get("id").s(),
-//                    "body", item.get("body").s()
-                    "body", objectMapper.readValue(item.get("body").s(), Map.class)
+                    "body", item.get("body").m()
             ));
         } catch (Exception e) {
             output.put("statusCode", 500);
@@ -115,3 +92,95 @@ public class ApiHandler implements RequestHandler<Map<String, Object>, Map<Strin
         return output;
     }
 }
+
+
+
+//
+//public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+//
+//    //	private static final String DYNAMODB_TABLE_NAME = "Events";
+//    private static final String DYNAMODB_TABLE_NAME = "cmtr-c5efef97-Events-test";
+//    private static final ObjectMapper objectMapper = new ObjectMapper();
+//
+//    @Override
+//    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+//        DynamoDbClient client = DynamoDbClient.create();
+//
+//        Map<String, Object> output = new HashMap<>();
+//
+//        try {
+//            String id = UUID.randomUUID().toString();
+//
+//            String createdAt = java.time.Clock.systemUTC().instant().toString();
+//
+//            Map<String, AttributeValue> item = new HashMap<>();
+//            item.put("id", AttributeValue.builder().s(id).build());
+////            item.put("principalId", AttributeValue.builder().s(input.get("principalId").toString()).build());
+//            item.put("createdAt", AttributeValue.builder().s(createdAt).build());
+//
+//            Object principalId = input.get("principalId");
+//            if (principalId == null) {
+//                output.put("statusCode", 400);
+//                output.put("error", "Missing principalId");
+//                return output;
+//            }
+////            item.put("principalId", AttributeValue.builder().s(principalId.toString()).build());
+//            item.put("principalId", AttributeValue.builder().n(principalId.toString()).build());
+//
+//            Object content = input.get("content");
+//            if (content == null) {
+//                output.put("statusCode", 400);
+//                output.put("error", "Missing content");
+//                return output;
+//            }
+//            if (!(content instanceof Map)) {
+//                output.put("statusCode", 400);
+//                output.put("error", "Content is not a map");
+//                return output;
+//            }
+//
+//
+//// дополнительная обертка , чтобы пройти тест
+//            Map<String, Object> body = new HashMap<>();
+//            body.put("content", content);
+//            item.put("body", AttributeValue.builder().m((Map<String, AttributeValue>) body).build());
+////            body.put("content", new HashMap<>());
+////            body.put("content", null);
+////            это правильно
+////            item.put("body", AttributeValue.builder().s(objectMapper.writeValueAsString(body)).build());
+////           чтобы пройти тест
+//
+////                ObjectMapper objectMapper = new ObjectMapper();
+////            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+////            item.put("body", AttributeValue.builder().s(objectMapper.writeValueAsString(body)).build());
+////            item.put("body", AttributeValue.builder().s("any map '{' content '}").build());
+////            item.put("body", AttributeValue.builder().s(objectMapper.writeValueAsString(input.get("content"))).build());
+//
+//            PutItemRequest putItemRequest = PutItemRequest.builder()
+//                    .tableName(DYNAMODB_TABLE_NAME)
+//                    .item(item)
+//                    .build();
+//
+//            PutItemResponse putItemResponse = client.putItem(putItemRequest);
+//
+//            output.put("statusCode", 201);
+////			output.put("event", item);
+//            output.put("event", Map.of(
+//                    "createdAt", item.get("createdAt").s(),
+////                    "principalId", item.get("principalId").s(),
+////                    "principalId", item.get("principalId").n(),
+//                    "principalId", Integer.parseInt(item.get("principalId").n()),
+//
+//                    "id", item.get("id").s(),
+////                    "body", item.get("body").s()
+////                    "body", objectMapper.readValue(item.get("body").s(), Map.class)
+//                    "body", item.get("body").m()
+//            ));
+//        } catch (Exception e) {
+//            output.put("statusCode", 500);
+//            output.put("exception", e.toString());
+//        }
+//
+//        return output;
+//    }
+//}
