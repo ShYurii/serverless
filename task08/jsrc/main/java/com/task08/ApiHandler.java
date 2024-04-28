81ffffe0b69f5a881141b776dcb59461bb826e4a
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-04-28 20:34:55 +0300

    task08 commit 2

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -3,7 +3,14 @@ package com.task08;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.task08.OpenMeteoAPI;
 
 import java.util.HashMap;
@@ -13,10 +20,23 @@ import java.util.Map;
 	roleName = "api_handler-role",
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
+		layers = {"sdk-layer"},
+		runtime = DeploymentRuntime.JAVA11,
+		architecture = Architecture.ARM64,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 
-
+@LambdaLayer(
+		layerName = "sdk-layer",
+//		libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
+		runtime = DeploymentRuntime.JAVA11,
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
 public class ApiHandler implements RequestHandler<Object, String> {
 	@Override
 	public String handleRequest(Object input, Context context) {
