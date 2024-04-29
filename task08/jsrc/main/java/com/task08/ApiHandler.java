0fd2022d4b1b9a789725393e56dd6872bc1d36ac
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-04-29 17:18:06 +0300

    task08 commit 3

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -5,29 +5,30 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.Architecture;
+//import com.syndicate.deployment.model.Architecture;
 import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
-import com.syndicate.deployment.model.RetentionSetting;
+//import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import org.example.OpenMeteoAPI;
+
 
 
 @LambdaHandler(lambdaName = "api_handler",
 	roleName = "api_handler-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
-		layers = {"sdk-layer"},
-		runtime = DeploymentRuntime.JAVA11,
-		architecture = Architecture.ARM64,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+//	isPublishVersion = true,
+//	aliasName = "${lambdas_alias_name}",
+		layers = {"sdk-layer"}
+//		runtime = DeploymentRuntime.JAVA11,
+//		architecture = Architecture.ARM64,
+//	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-
 @LambdaLayer(
 		layerName = "sdk-layer",
-//		libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
+		libraries = {"lib/open_meteo_api-1.0-SNAPSHOT.jar"},
 		runtime = DeploymentRuntime.JAVA11,
-		architectures = {Architecture.ARM64},
+//		architectures = {Architecture.ARM64},
 		artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
@@ -37,7 +38,7 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 public class ApiHandler implements RequestHandler<Object, String> {
 	@Override
 	public String handleRequest(Object input, Context context) {
-		com.task08.OpenMeteoAPI api = new com.task08.OpenMeteoAPI(52.5200, 13.4050); // Berlin, Germany
+		OpenMeteoAPI api = new OpenMeteoAPI(52.5200, 13.4050); // Berlin, Germany
 		String forecast;
 		try {
 			forecast = api.getWeatherForecast();
