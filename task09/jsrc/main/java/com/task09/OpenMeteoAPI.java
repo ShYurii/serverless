009ea48f247289736905b15773f7ea628af84f95
YuriiShevchenko <yurii_shevchenko@epam.com>
2024-05-01 16:06:13 +0300

    task09 commit 4

--- task09/jsrc/main/java/com/task09/OpenMeteoAPI.java
+++ task09/jsrc/main/java/com/task09/OpenMeteoAPI.java
@@ -1,6 +1,9 @@
 package com.task09;
 
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,20 +11,26 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
 public class OpenMeteoAPI {
-    private static final String BASE_URL = "http://api.open-meteo.com/v1/forecast";
-    private double latitude;
-    private double longitude;
+    public String callApi() {
+            StringBuilder response = new StringBuilder();
+            try {
+                URL url = new URL("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
 
-    public OpenMeteoAPI(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+
+                reader.close();
+                System.out.println("API Response: " + response);
+                connection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return response.toString();
+        }
     }
-
-    public String getWeatherForecast() throws Exception {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://api.open-meteo.com/v1/forecast?latitude=" + this.latitude + "&longitude=" + this.longitude + "&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")).header("accept", "application/json").build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        return (String)response.body();
-    }
-}
-
